161ed7d7a58a778feb3a2eb1c171eaf82b6d94ff
Shubham Mehar <shubhammehar101@gmail.com>
2023-12-28 01:46:53 +0530

    code improved

--- src/components/BMICalculator.jsx
+++ src/components/BMICalculator.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import InputField from './InputField';
 import './BMICalculator.css';
 
@@ -6,40 +6,39 @@ const BMICalculator = () => {
   const [weight, setWeight] = useState('0');
   const [height, setHeight] = useState('0');
   const [validationErrors, setValidationErrors] = useState({});
-  let calculatedBMI = null;
+  const [canShow, setCanShow] = useState(false);
 
   const validateInput = () => {
     const errors = {};
     const validWeight = parseFloat(weight);
     const validHeight = parseFloat(height);
-    if(isNaN(validWeight) || validWeight < 1.0 || validWeight > 300.0){
+    if (isNaN(validWeight) || validWeight < 1.0 || validWeight > 300.0) {
       errors.weight = "Valid weight value ranges from 1.0-300.0";
     }
-    if(isNaN(validHeight) || validHeight < 0.1 || validHeight > 3.0){
+    if (isNaN(validHeight) || validHeight < 0.1 || validHeight > 3.0) {
       errors.height = "Valid height value ranges from 0.1-3.0";
     }
+
     setValidationErrors(errors);
     return Object.keys(errors).length === 0;
   };
 
   const calculateBMI = () => {
-    const validWeight = parseFloat(weight);
-    const validHeight = parseFloat(height);
-    if(validateInput()){
-      calculatedBMI = (validWeight / (validHeight * validHeight)).toFixed(1);
-    } else{
-      calculatedBMI = null;
-    }
-};
+    const Weight = parseFloat(weight);
+    const Height = parseFloat(height);
+    return (Weight / (Height * Height)).toFixed(1);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    calculateBMI();
-    if (calculatedBMI !== null) {
-      const resultElement = document.getElementById('calculated-bmi');
-      resultElement.innerText = `Your BMI is: ${calculatedBMI}`;
+    if (validateInput()) {
+      setCanShow(true);
     }
   };
+
+  useEffect(() => {
+    setCanShow(false);
+  }, [weight, height]);
 
   return (
     <div>
@@ -60,7 +59,11 @@ const BMICalculator = () => {
           />
         </div>
         <button className="calculate-button">Calculate BMI</button>
-        <p id="calculated-bmi" className="result-text"></p>
+        {canShow && (
+          <p data-testid="calculated-bmi" id="calculated-bmi" className="result-text">
+            {`Your BMI is: ${calculateBMI()}`}
+          </p>
+        )}
       </form>
     </div>
   );
