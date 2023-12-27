278c1028d063c985ec1fae3e9d87b3df22cfbfb6
Shubham Mehar <shubhammehar101@gmail.com>
2023-12-28 01:36:15 +0530

    id added

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,10 +1,13 @@
 import './BMICalculator.css';
 
 const InputField = ({label, value, onChange,error}) => {
+  const inputId = label.toLowerCase();
+
   return (
     <div> 
-        <label >{label}:</label>
+        <label  htmlFor={inputId}>{label}:</label>
         <input className="input-field" 
+                id={inputId}
             type='number' value={value}  
             onChange={(e)=>{ onChange(e.target.value);}}
         />
